b954a09e972dc8c7eacd5d6275fd977d94384396
kadelcode <kadelcode@gmail.com>
2025-03-12 09:41:51 +0100

    New tailwind config file

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+export default {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1D4ED8', // Replace with your preferred color
+        },
+      },
+    },
+    corePlugins: {
+      preflight: false, // Optional test if Safari has styling issues
+    },
+  };
+  
