762274723122df87f6fd8d1e6f07f5598f173609
kadelcode <kadelcode@gmail.com>
2025-03-12 13:14:08 +0100

    Call To Action Section

---
+++ src/components/sections/CTA.tsx
@@ -0,0 +1,14 @@
+import { Button } from "../ui/button";
+
+export default function CTA() {
+    return (
+        <section className="py-20 px-4 md:px-0 bg-blue-50">
+            <div className="container mx-auto text-center">
+                <h2 className="text-3xl font-semibold text-[#193cb8] mb-8">Ready to Experience Smarter Conversations?</h2>
+                <Button className="bg-[#155dfc] hover:bg-[#1447e6] text-white text-lg font-bold py-8 px-12 rounded full">
+                    Start Chatting Now
+                </Button>
+            </div>
+        </section>
+    )
+}
