9f37d80732ddf54f2cbde11fee4dbe9d79ff9569
kadelcode <kadelcode@gmail.com>
2025-03-12 11:57:43 +0100

    Update shadow color for IOS Safari

--- src/components/sections/Features.tsx
+++ src/components/sections/Features.tsx
@@ -35,9 +35,9 @@ export function Features() {
         <section className="py-16 px-4 md:px-0 bg-white">
             <div className="container mx-auto">
                 <h2 className="text-3xl font-semibold text-center text-[#1e2939] mb-12">Key Features and Benefits</h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 bg-[#3d50f7]">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
                     { features.map((feature, index) => (
-                        <div className="text-center p-6 shadow-md rounded-md" key={index}>
+                        <div className="text-center p-6 shadow-md rounded-md shadow-[#d1d5dc]" key={index}>
                             {feature.icon}
                             <h3 className="text-xl font-semibold text-[#1e2939] mb-2">{feature.title}</h3>
                             <p className="text-[#4a5565]">{feature.description}</p>
