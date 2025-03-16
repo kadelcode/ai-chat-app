1dfaf96310202b409fb6addc580657774dddcc73
kadelcode <kadelcode@gmail.com>
2025-03-16 08:14:50 +0100

    Skeleton components

---
+++ src/components/SkeletonLogin.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const SkeletonLogin = () => {
+  return (
+    <div className="w-full max-w-md p-6 bg-gray-300 rounded-lg shadow-md">
+      <Skeleton className="h-8 w-1/3 mx-auto mb-4" />
+      <div className="mb-4">
+        <Skeleton className="h-5 w-20 mb-2" /> {/* Label */}
+        <Skeleton className="h-10 w-full rounded-lg" /> {/* Input */}
+      </div>
+      <div className="mb-4">
+        <Skeleton className="h-5 w-20 mb-2" /> {/* Label */}
+        <Skeleton className="h-10 w-full rounded-lg" /> {/* Input */}
+      </div>
+      <Skeleton className="h-10 w-full rounded-lg" /> {/* Button */}
+    </div>
+  );
+};
+
+export default SkeletonLogin;
